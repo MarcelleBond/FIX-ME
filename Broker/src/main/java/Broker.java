55760743a3aaecaeb036107aa915b7d17fc89e95
Lunga MAKWAKWA <lmakwakw@c4r14s4.wethinkcode.co.za>
2019-09-04 15:07:40 +0200

    broker push

--- Broker/src/main/java/Broker.java
+++ Broker/src/main/java/Broker.java
@@ -1,117 +1,126 @@
-import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class Broker {
-    private int id;
-    // initialize socket and input output streams
-    private Socket socket = null;
-    private Scanner userInput = null;
-    private DataInputStream serverInput = null;
-    private DataOutputStream output = null;
 
+    public static void validate_input(String input) {
 
-    public Broker(String address, int port) {
-        try {
-            socket = new Socket(address, port);
-            System.out.println("Connected");
+        String[] split_input;
 
-            // takes input from terminal
-            userInput = new Scanner(System.in);
+        //split the input by spaces
+        split_input = input.split(" ");
 
-            // gets input from the router
-            serverInput = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-            id = Integer.parseInt(serverInput.readUTF());
-
-            System.out.print(id);
-            // sends output to the socket
-            output = new DataOutputStream(socket.getOutputStream());
-        } catch (
-                UnknownHostException u) {
-            System.out.println(u);
-        } catch (
-                IOException i) {
-            System.out.println(i);
+        if (!split_input[0].toLowerCase().equals("buy") && !split_input[0].toLowerCase().equals("sell")) {
+            System.out.println("State whether you buying or selling");
         }
 
-    }
-
-    public void sendMessages() {
-        String line = "";
-
-        // keep reading until "Over" is input
-        while (!line.equals("Over")) {
-            try {
-                line = userInput.nextLine();
-                output.writeUTF(line);
-            } catch (IOException i) {
-                System.out.println(i);
-            }
-        }
-        System.out.print("Im out\n");
-        // close the connection
-        try {
-            userInput.close();
-            serverInput.close();
-            output.close();
-            socket.close();
-        } catch (IOException i) {
-            System.out.println(i);
-        }
-    }
-
-    public void reciveMessage() {
-        String line = "";
-        // reads message from client until "Over" is sent
-        while (true) {
-            try {
-                line = serverInput.readUTF();
-                System.out.println(line);
-
-            } catch (IOException i) {
-                System.out.println(i);
-            }
-        }
-    }
-
-    public void validate_input(String input)
-    {
-         String[] split_input;
-        //buy or sell string to lower
-        //
-        //buy zar -2000 2 10001
-
-        //instrument loop through instrument list to see if it exists
-        split_input = input.trim().split(" ");
-        if (split_input[0].toLowerCase() != "buy" || split_input[0].toLowerCase() != "sell")
-        {
-            System.out.println("State weather you are buying or selling niqqah");
-        }
-
-        for (int i = 0; i < split_input[1].length(); i++) {
-            if(!Character.isDigit(split_input[1].charAt(i))) {
+        for (int i = 0; i < split_input[2].length(); i++) {
+            if (!Character.isDigit(split_input[2].charAt(i))) {
                 System.out.println("Invalid Price");
             }
         }
 
-        for (int i = 0; i < split_input[2].length(); i++) {
-            if(!Character.isDigit(split_input[2].charAt(i))) {
+        for (int i = 0; i < split_input[3].length(); i++) {
+            if (!Character.isDigit(split_input[3].charAt(i))) {
                 System.out.println("Invalid Quantity");
             }
         }
 
-        for (int i = 0; i < split_input[2].length(); i++) {
-            if(!Character.isDigit(split_input[2].charAt(i))) {
+        for (int i = 0; i < split_input[4].length(); i++) {
+            if (!Character.isDigit(split_input[4].charAt(i))) {
                 System.out.println("Invalid Market ID");
             }
         }
-
-
     }
+
+    public static int GenerateCheckSum(String message) {
+        int sum = 0;
+        int length;
+        int i;
+
+        length = message.length();
+        for (i = 0; i < length; i++) {
+            int value = message.charAt(i);
+            sum += value;
+        }
+        int checkSum = sum % 256;
+        return(checkSum);
+    }
+
+    public static String checkSumEncrypt(String message)
+    {
+        int checkSum = GenerateCheckSum(message);
+        return (message + " " + checkSum);
+    }
+
+
+
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+        final Scanner scn = new Scanner(System.in);
+        final int id;
+        // getting localhost ip
+        InetAddress ip = InetAddress.getByName("localhost");
+
+        // establish the connection
+        Socket s = new Socket(ip, 5000);
+
+        // obtaining input and out streams
+        final DataInputStream dis = new DataInputStream(s.getInputStream());
+        id = Integer.parseInt(dis.readUTF());
+        System.out.println(id);
+        final DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        // sendMessage thread
+        Thread sendMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    // read the message to deliver.
+                    String msg = scn.nextLine();
+                    validate_input(msg);
+                    String msg2 = checkSumEncrypt(msg);
+
+                    try {
+                        // write on the output stream
+                        dos.writeUTF(id + " " + msg2);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // readMessage thread
+        Thread readMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        // read the message sent to this client
+                        String msg = dis.readUTF();
+                        System.out.println(msg);
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        sendMessage.start();
+        readMessage.start();
+    }
+
 }
